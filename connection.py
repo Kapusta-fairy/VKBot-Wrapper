51f4a38cda9af5e1224a7763cdd5c4ff7ce11616
Kapusta-fairy <dknenorm63@gmail.com>
2022-09-28 11:07:29 +0300

    edit README.md

--- connection.py
+++ connection.py
@@ -1,6 +1,8 @@
 from vk_api.bot_longpoll import VkBotLongPoll
 from vk_api.vk_api import VkApiGroup
+token = 'токен сообщества'
+group_id = 'id сообщества'
 
-vk_session = VkApiGroup(token='токен сообщества')  # тут мы создаем сессию, для этого нужен token твоего сообщества
+vk_session = VkApiGroup(token=token)  # тут мы создаем сессию, для этого нужен token твоего сообщества
 vk = vk_session.get_api()  # получаем само api ВК для сессии, чтобы в дальнейшем взаимодействовать с ним через код
-longpoll = VkBotLongPoll(vk_session, 212138773)  # получаем longpoll, чтобы принимать все события
+longpoll = VkBotLongPoll(vk_session, group_id)  # получаем longpoll, чтобы принимать все события
